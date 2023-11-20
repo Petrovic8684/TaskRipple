17e9d7abf0646fddf7ccaf2e5649d13d4cb221a9
Petrovic8684 <jovan8684@gmail.com>
2023-11-20 17:12:48 +0100

    Deployed to render and fixed cors settings

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,3 @@
-const allowedOrigins = [
-  "https://taskripple-api.onrender.com/",
-  "3.75.158.163",
-  "3.125.183.140",
-  "35.157.117.28",
-  "https://task-ripple.vercel.app",
-];
+const allowedOrigins = ["*"];
 
 export default allowedOrigins;
