0998b60a50725941477686a9b515efcc0e0b2580
Petrovic8684 <jovan8684@gmail.com>
2023-11-20 16:42:07 +0100

    Fixed vercel deployment 17

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ["https://taskripple-api.onrender.com/"];
+const allowedOrigins = [
+  "https://taskripple-api.onrender.com/",
+  "3.75.158.163",
+  "3.125.183.140",
+  "35.157.117.28",
+];
 
 export default allowedOrigins;
